2638d47cd983faad812764a9666adcdfd33ad779
bassistcz <86234105+bassistcz@users.noreply.github.com>
2024-10-30 06:41:47 +0000

    Update CodeDemo.cpp

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -6,6 +6,26 @@
 
 int main(){
     
+    int a = 1023;
+    bool flag = false;
+    char lttr = 'd';
+    
+    if (a > 1000)
+        std::cout << "a is greater than 1000" << std::endl;
+
+    if ((a % 2))
+        std::cout << "a is odd" << std::endl;
+    else
+        std::cout << "a is even" << std::endl;
+        
+    std::cout << "The letter " << lttr << " is";
+    if (lttr != 'a' && lttr != 'e' && lttr != 'i' && lttr != 'o' && lttr != 'u' && 
+        lttr != 'A' && lttr != 'E' && lttr != 'I' && lttr != 'O' && lttr != 'U')
+        std::cout << "not ";
+    std::cout << "a vowel" << std::endl;
+    
+    
+    
     std::cout << std::endl << std::endl;
     return (0);
 }
